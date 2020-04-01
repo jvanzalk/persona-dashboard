2f66b615238734e791b161199479628ef1b6efb9
John van Zalk <jvanzalk@SEPA.local>
2020-04-01 08:58:46 -0400

    all files

---
+++ app.py
@@ -0,0 +1,83 @@
+import os
+
+from flask import Flask, render_template
+
+# import SQL Alchemy
+import sqlalchemy
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+# from config import dbuser, dbpwd, dbhost, dbport, dbname
+
+#import config files
+dbuser = os.environ.get("dbuser")
+dbpwd = os.environ.get("dbpwd")
+dbhost = os.environ.get("dbhost")
+dbport = os.environ.get("dbport")
+dbname = os.environ.get("dbname")
+
+
+# # configure Postgresql connection
+
+engine = create_engine(f'postgresql://{dbuser}:{dbpwd}@{dbhost}:{dbport}/{dbname}')
+conn = engine.connect()
+
+# # Initialize Flask application
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Reestablish DB connection
+    conn = engine.connect()
+
+    ## POPULATION DATA ##
+    population = pd.read_sql("SELECT * FROM population", conn)
+
+    data = population.to_dict('records')
+    
+    # population = json.dumps(population, indent=2)
+    # data = [population]
+
+    ## AVG ENGAGEMENT DATA ##
+    avg_engage = pd.read_sql("SELECT * FROM avg_engage", conn)
+
+    avg_engage = avg_engage.to_dict('records')
+
+    ## AVG ENGAGEMENT TYPE ##
+    engage_type = pd.read_sql("SELECT * FROM engage_type", conn)
+
+    engage_type = engage_type.to_dict('records')
+
+    ## ENGAGEMENT STATUS (RF) ##
+    
+    recfreq = pd.read_sql("SELECT * FROM recfreq", conn)
+
+    recfreq = recfreq.to_dict('records')
+
+    ## NEW CONTACTS ##
+    
+    pop_growth = pd.read_sql("SELECT * FROM pop_growth", conn)
+
+    pop_growth = [{'persona':'Claire','y':pop_growth.loc[pop_growth.persona == 'Claire',].values.flatten().tolist()[1:],'x':list(pop_growth.columns)[1:]},
+    {'persona':'Dan','y':pop_growth.loc[pop_growth.persona == 'Dan',].values.flatten().tolist()[1:],'x':list(pop_growth.columns)[1:]},
+    {'persona':'Lynn','y':pop_growth.loc[pop_growth.persona == 'Lynn',].values.flatten().tolist()[1:],'x':list(pop_growth.columns)[1:]},
+    {'persona':'Maya','y':pop_growth.loc[pop_growth.persona == 'Maya',].values.flatten().tolist()[1:],'x':list(pop_growth.columns)[1:]},
+    {'persona':'Rey','y':pop_growth.loc[pop_growth.persona == 'Rey',].values.flatten().tolist()[1:],'x':list(pop_growth.columns)[1:]}]
+
+    ## TOP ITEMS ##
+    
+    top_items = pd.read_sql("SELECT * FROM top_items", conn)
+
+    top_items = top_items.to_dict('records')
+
+    ## TOP CAMPAIGNS ##
+    
+    top_campaigns = pd.read_sql("SELECT * FROM top_campaigns", conn)
+
+    top_campaigns = top_campaigns.to_dict('records')
+
+    return render_template('test.html', data=data, avg_engage=avg_engage, engage_type=engage_type, recfreq=recfreq, pop_growth=pop_growth, top_campaigns=top_campaigns, top_items=top_items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/test.js
@@ -0,0 +1,275 @@
+
+function myFunc() {
+    return popData
+}
+console.log(popData);
+
+var sortedpops = popData.sort((b, a) => b.count - a.count);
+
+var data = [
+    {
+        y: ["Maya ","Rey ","Lynn ","Claire ","Dan "],  
+        x: [sortedpops[0].count,sortedpops[1].count,sortedpops[2].count,sortedpops[3].count,sortedpops[4].count],
+        type: "bar",
+        orientation: 'h',
+        marker: {
+            color: 'rgba(0,117,201,.8)',
+            line: {
+                color: 'rgb(0,74,152)',
+                width: 2
+            }
+        }
+    }
+];               
+
+var layout = {
+    title: "Population",
+    xaxis: { title: "Individuals"},
+    yaxis: {
+        tickfont: {
+        size: 18,
+        }
+    }
+};
+            
+// Plot the chart to a div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
+
+function myFunc() {
+    return avg_engage
+}
+
+console.log(avg_engage);
+
+var data2 = [
+    {
+      y: [avg_engage[3].persona,avg_engage[4].persona,avg_engage[2].persona,avg_engage[0].persona,avg_engage[1].persona],  
+      x: [avg_engage[3].engagement,avg_engage[4].engagement,avg_engage[2].engagement,avg_engage[0].engagement,avg_engage[1].engagement],
+      type: "bar",
+      orientation: 'h',
+      marker: {
+        color: 'rgba(0,117,201,.8)',
+        line: {
+          color: 'rgb(0,74,152)',
+          width: 2
+        }
+      }
+    }
+  ];
+  
+  var layout = {
+    title: "Average Engagements Per Individual",
+    xaxis: { title: "Engagements"},
+    yaxis: {
+      showticklabels: false
+    }
+  };
+    
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("plot2", data2, layout)
+
+
+
+/////ENGAGEMENT TYPE PLOT/////
+
+function myFunc() {
+    return engage_type
+}
+
+console.log(engage_type);
+
+var event = {
+    y: [engage_type[3].persona, engage_type[4].persona, engage_type[2].persona, engage_type[0].persona, engage_type[1].persona],
+    x: [engage_type[3].Event, engage_type[4].Event, engage_type[2].Event, engage_type[0].Event, engage_type[1].Event],
+    name: 'Event',
+    type: 'bar',
+    orientation: 'h',
+    marker: {
+      color: 'rgba(34, 58, 111,.9)',
+      line: {
+        color: 'rgb(34, 58, 111)',
+        width: 2
+      }
+    }
+  };
+  
+  var publication = {
+    y: [engage_type[3].persona, engage_type[4].persona, engage_type[2].persona, engage_type[0].persona, engage_type[1].persona],
+    x: [engage_type[3].Publication, engage_type[4].Publication, engage_type[2].Publication, engage_type[0].Publication, engage_type[1].Publication],
+    name: 'Publication',
+    type: 'bar',
+    orientation: 'h',
+    marker: {
+      color: 'rgba(0,117,201,.8)',
+      line: {
+        color: 'rgb(0,74,152)',
+        width: 2
+      }
+    }
+  };
+  
+  var wg = {
+    y: [engage_type[3].persona, engage_type[4].persona, engage_type[2].persona, engage_type[0].persona, engage_type[1].persona],
+    x: [engage_type[3].WorkingGroup, engage_type[4].WorkingGroup, engage_type[2].WorkingGroup, engage_type[0].WorkingGroup, engage_type[1].WorkingGroup],
+    name: 'Working Group',
+    type: 'bar',
+    orientation: 'h',
+    marker: {
+      color: 'rgba(230,75,5,.8)',
+      line: {
+        color: 'rgb(230,75,5)',
+        width: 2
+      }
+    }
+  };
+  
+  var data3 = [event, publication, wg];
+  
+  var layout = {
+    title: "Total Engagements by Type",
+    xaxis: { title: "Percent"},
+    barmode: 'stack',
+    yaxis: {
+      showticklabels: false
+    }
+  };
+  
+  Plotly.newPlot('plot3', data3, layout);
+
+/////POP GROWTH PLOT/////
+
+
+function val() {  
+
+    d = document.getElementById("Persona").value;
+
+    sel = 0
+
+    if (d == 'Claire') {
+    sel = 0;
+    } else if (d == 'Dan') {
+    sel = 1;
+    } else if (d == 'Lynn') {
+    sel = 2;
+    } else if (d == 'Maya') {
+    sel = 3;
+    } else {
+    sel = 4;
+    };
+
+
+    function myFunc() {
+        return pop_growth
+    };
+
+    console.log(pop_growth);
+
+    var data4 = [{
+    x: pop_growth[sel].x,
+    y: pop_growth[sel].y,
+    name: pop_growth[sel].persona,
+    line: {color: '#223a6f'}
+    }];
+
+    var layout = {
+    title: 'New Contacts Per Quarter',
+    };
+
+
+    Plotly.newPlot('plot4', data4, layout);
+
+    ///// RF PLOT /////
+
+    function myFunc() {
+        return recfreq
+    };
+
+    var data = [{
+    values: [recfreq[sel].Active,recfreq[sel].New,recfreq[sel].Lapsed,recfreq[sel].Lost],
+    labels: ['Active', 'New', 'Lapsed','Lost'],
+    type: 'pie',
+    marker: {
+        colors: [
+        '#223a6f',
+        '#00a0df',
+        '#d0d0ce',
+        '#e64b38'
+        ]
+    }
+    }];
+    
+    var layout = {
+    title: 'Engagement Status'
+    };
+    
+    Plotly.newPlot('plot5', data, layout);
+
+    ///// TOP ITEMS TABLE /////
+
+    function myFunc() {
+        return top_items
+    };
+
+    //filter top_items by persona (d)
+    filtered_items = top_items.filter( i => d.includes( i.persona ) );
+
+    // Target the Table you want to insert the Data to
+    // var results=document.getElementById('Items');
+
+    var tableRef = document.getElementById('Items').getElementsByTagName('tbody')[0];
+
+    for(var i = document.getElementById("Items").getElementsByTagName('tbody')[0].rows.length; i > 0;i--)
+    {
+        document.getElementById("Items").getElementsByTagName('tbody')[0].deleteRow(i -1);
+    }
+
+    for(var obj in filtered_items){
+        // Insert a row in the table at row index 0
+        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+        // Insert a cell in the row at index 0
+        var newCell  = newRow.insertCell(0);
+
+        // Append a text node to the cell
+        var newText  = document.createTextNode(filtered_items[obj].engagement)
+        newCell.appendChild(newText);
+
+        var newCell  = newRow.insertCell(1);
+
+        var newText  = document.createTextNode(filtered_items[obj].count)
+        newCell.appendChild(newText);
+    };
+
+    ///// TOP CAMPAIGNS TABLE /////
+
+    function myFunc() {
+        return top_campaigns
+    };
+    
+    //filter top_items by persona (d)
+    filtered_campaigns = top_campaigns.filter( i => d.includes( i.persona ) );
+
+    var tableRef = document.getElementById('Campaigns').getElementsByTagName('tbody')[0];
+
+    for(var i = document.getElementById("Campaigns").getElementsByTagName('tbody')[0].rows.length; i > 0;i--)
+    {
+        document.getElementById("Campaigns").getElementsByTagName('tbody')[0].deleteRow(i -1);
+    }
+
+    for(var obj in filtered_campaigns){
+        // Insert a row in the table at row index 0
+        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+        var newCell  = newRow.insertCell(0);
+        var newText  = document.createTextNode(filtered_campaigns[obj].campaign)
+        newCell.appendChild(newText);
+
+        var newCell  = newRow.insertCell(1);
+        var newText  = document.createTextNode(filtered_campaigns[obj].count)
+        newCell.appendChild(newText);
+    };
+}; 
+
+var select = document.getElementById("Persona");
+select.onchange = val;
+window.onload = val;
